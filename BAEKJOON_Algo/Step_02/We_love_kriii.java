a4b9a5813e0da3eadaf8b5607d118c041e595d32
oilsok87 <cfd.magician@gmail.com>
2021-07-03 00:10:34 +0900

    step2

---
+++ BAEKJOON_Algo/Step_02/We_love_kriii.java
@@ -0,0 +1,8 @@
+package BAEKJOON_Algo.Step_02;
+
+public class We_love_kriii {
+    public static void main(String [] args){
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
